f188da6be250b99263229c3b0503f91f29252399
touch <tttouch1122@gmail.com>
2023-11-28 22:03:45 +0700

    -ใน  class main มีแค่เรียกใช้ -class Character มี getItem ในการเลือกสิ่งของ (Sword or Shield) updateState เอาไว้เปลี่ยนค่าต่างๆ showStatus โชว์ค่าก่อนเลือกต่างๆ showStatus2 โชว์ค่าหลังเลือก showStatus3 โชว์ค่าหลังreset showError โชว์ค่าตอนใส่ผิด reset เซ็ตค่าเป็นเริ่มต้น

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         Character p1 = new Character();
-        p1.showStatus();
+
         p1.getItem();
+
+        Character p2 = new Character();
+        p2.getItem();
+
+//        p1.Attack(p2);
+//        p2.showStatusAfterAttack();
+
+        p1.Defend(p2);
+        p1.showStatusAfterAttack();
+        p2.showStatusAfterAttack();
+
     }
 }
